b19a934b01a660e61c21bcf2b0e615cbed149548
VAfonin <vaafonin@vtb.ru>
2022-08-17 07:33:24 +0300

    Add impl r2dbc.

---
+++ r2dbc/src/main/java/com/example/r2dbc/model/Book.java
@@ -0,0 +1,60 @@
+package com.example.r2dbc.model;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.util.Objects;
+
+@Table
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Book {
+
+    @Id
+    private Long id;
+
+    private String name;
+
+    private String isbn;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(name, book.name) && Objects.equals(isbn, book.isbn);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, isbn);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+}
